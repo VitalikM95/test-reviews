16165625b4894239e1d4fe0084c9cf46f1b9b567
Vitalik <marychvitalik95@gmail.com>
2025-07-17 16:08:12 +0300

    delpoy

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -3,44 +3,44 @@ import { Carousel } from '../components/Carousel'
 
 const CARDS_DATA = [
   {
-    logo: '../assets/logo2.jpg',
+    logo: '/test-reviews/logo2.jpg',
     text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem obcaecati corporis et optio repellendus autem maxime eligendi saepe mollitia nihil.',
-    avatar: '../assets/avatar2.jpg',
+    avatar: '/test-reviews/avatar2.jpg',
     name: 'Ethan Morgan',
     position: 'Founder and CEO, Serence Living Products',
   },
   {
-    logo: '../assets/logo3.jpg',
+    logo: '/test-reviews/logo3.jpg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit animi quos qui explicabo fuga aut quis vel consectetur.',
-    avatar: '../assets/avatar1.jpg',
+    avatar: '/test-reviews/avatar1.jpg',
     name: 'Olivia Hayes',
     position: 'Owner, Starlight Creations',
   },
   {
-    logo: '../assets/logo1.jpg',
+    logo: '/test-reviews/logo1.jpg',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint excepturi autem reprehenderit ratione perspiciatis, cupiditate ducimus dignissimos dolorem reiciendis',
-    avatar: '../assets/avatar3.jpg',
+    avatar: '/test-reviews/avatar3.jpg',
     name: 'Alexander Reed',
     position: 'Co-Founder, Opulent Living Group',
   },
   {
-    logo: '../assets/logo3.jpg',
+    logo: '/test-reviews/logo3.jpg',
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit animi quos qui explicabo fuga aut quis vel consectetur.',
-    avatar: '../assets/avatar1.jpg',
+    avatar: '/test-reviews/avatar1.jpg',
     name: 'Olivia Hayes',
     position: 'Owner, Starlight Creations',
   },
   {
-    logo: '../assets/logo1.jpg',
+    logo: '/test-reviews/logo1.jpg',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint excepturi autem reprehenderit ratione perspiciatis, cupiditate ducimus dignissimos dolorem reiciendis',
-    avatar: '../assets/avatar3.jpg',
+    avatar: '/test-reviews/avatar3.jpg',
     name: 'Alexander Reed',
     position: 'Co-Founder, Opulent Living Group',
   },
   {
-    logo: '../assets/logo2.jpg',
+    logo: '/test-reviews/logo2.jpg',
     text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem obcaecati corporis et optio repellendus autem maxime eligendi saepe mollitia nihil.',
-    avatar: '../assets/avatar2.jpg',
+    avatar: '/test-reviews/avatar2.jpg',
     name: 'Ethan Morgan',
     position: 'Founder and CEO, Serence Living Products',
   },
@@ -50,7 +50,7 @@ export const Reviews = () => {
   return (
     <section
       className="w-screen h-screen bg-cover bg-center overflow-hidden flex items-center justify-center py-20"
-      style={{ backgroundImage: 'url(../assets/bg.jpg)' }}
+      style={{ backgroundImage: 'url(/test-reviews/bg.jpg)' }}
     >
       <div className="max-w-[1240px] w-full mx-auto flex flex-col items-center justify-between h-full">
         <h1 className="text-4xl font-bold mt-8 mb-12 text-center">
